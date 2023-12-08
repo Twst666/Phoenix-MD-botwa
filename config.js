37e7b2e7c392a928ef55e49deb8c3f616e808575
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-12-08 15:59:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 const toBool = (x) => x === 'true';
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'Phoenix~Z5UpFfE3',
+    SESSION_ID: process.env.SESSION_ID || 'Phoenix~UnMG3VSF',
     HANDLERS: process.env.HANDLER || '.',
     BOT_NAME: process.env.BOT_NAME || 'Phoenix-MD',
     SUDO: process.env.SUDO || '917025673121',
