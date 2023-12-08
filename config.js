6eb9f935a809aa6b4c7d9ffbe331da5bb5e1637b
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-12-08 19:41:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,6 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID || 'Phoenix~WY6qDSff',
     HANDLERS: process.env.HANDLER || '.',
     BOT_NAME: process.env.BOT_NAME || 'Phoenix-MD',
-    SUDO: process.env.SUDO || '917025673121',
+    SUDO: process.env.SUDO || '919074692450',
+    STICKER_DATA: process.env.STICKER_DATA || '🎯𝙿𝚑𝚘𝚎𝚗𝚒𝚡-𝙼𝙳;𝙰𝚋𝚑𝚒𝚜𝚑𝚎𝚔 𝚂𝚞𝚛𝚎𝚜𝚑☘️',
 };
