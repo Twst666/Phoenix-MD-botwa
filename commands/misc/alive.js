a17f68f705a11e07f2401fa61e002b118829f53f
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-12-09 13:54:52 +0530

    Create alive.js

---
+++ commands/misc/alive.js
@@ -0,0 +1,42 @@
+module.exports = {
+    name: "alive",
+    category: "main",
+    desc: "Bot Chatho Illeuo Enn Nokan",
+    async exec({ msg, client }) {
+        const image = 'https://i.ibb.co/tHWJrz3/IMG-20231128-WA0005.jpg';
+        const thumb = "https://i.ibb.co/tHWJrz3/IMG-20231128-WA0005.jpg";
+    
+        const number = client.user.id; // Changed from msg.user.jid to client.user.id
+        const sourceUrl = 'https://github.com/AbhishekSuresh2/Phoenix-MD';
+
+        const linkPreview = {
+            title: "I'M Alive Now",
+            body: "𝙿𝚑𝚘𝚎𝚗𝚒𝚡-𝙼𝙳",
+            thumbnail: image,
+            mediaType: 1,
+            mediaUrl: sourceUrl,
+            sourceUrl: sourceUrl,
+            showAdAttribution: false,
+            renderLargerThumbnail: false
+        };
+
+        msg.quoted = {
+            key: {
+                fromMe: false,
+                participant: "0@s.whatsapp.net",
+                remotejid: "status@broadcast"
+            },
+            message: {
+                contactMessage: {
+                    displayName: `${msg.pushName}`,
+                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:XL;${client.user.name},;;;\nFN:${client.user.name},\nitem1.TEL;waid=919074692450:919074692450\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
+                    jpegThumbnail: thumb,
+                }
+            }
+        };
+
+        const text = `testingg`;
+
+        await client.sendMessage(msg.from, { text: text, contextInfo: { externalAdReply: linkPreview } }, { quoted: msg.quoted || '' });
+    }
+};
