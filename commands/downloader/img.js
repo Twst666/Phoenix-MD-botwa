05e7a1df6db49e07e2c79d88a8bea68b87270640
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-12-09 15:20:25 +0530

    Create img.js

---
+++ commands/downloader/img.js
@@ -0,0 +1,38 @@
+async function gimage(query, amount = 5) {
+  let list = [];
+  return new Promise((resolve, reject) => {
+    gis(query, async (error, result) => {
+      for (
+        var i = 0;
+        i < (result.length < amount ? result.length : amount);
+        i++
+      ) {
+        list.push(result[i].url);
+      }
+      resolve(list);
+    });
+  });
+}
+
+module.exports = {
+  name: "gimage",
+  category: "main",
+  desc: "Get Google Images",
+  async exec({ message, match }) {
+    if (!match)
+      return await message.sendMessage(
+        "_Enter A Text And Number Of Images You Want_\n_📌 Example:_ *Phoenix MD,6*"
+      );
+
+    let [query, amount] = match.split(",");
+    let result = await gimage(query, amount);
+
+    await message.sendMessage(
+      `_Downloading... *${amount || 5}* Images For *${query}*_`
+    );
+
+    for (let imageUrl of result) {
+      await message.sendFromUrl(imageUrl);
+    }
+  },
+};
