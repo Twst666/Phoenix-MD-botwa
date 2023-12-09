e7d9b865dab759aea643381897d31f105a2b393a
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-12-09 14:26:38 +0530

    Update alive.js

--- commands/misc/alive.js
+++ commands/misc/alive.js
@@ -1,23 +1,14 @@
-const getBuffer = require('get-buffer');
-
 module.exports = {
     name: "alive",
     category: "main",
     desc: "Bot Chatho Illeuo Enn Nokan",
     async exec({ msg, client }) {
-        const image = 'https://i.ibb.co/tHWJrz3/IMG-20231128-WA0005.jpg';
-        const thumb = "https://i.ibb.co/tHWJrz3/IMG-20231128-WA0005.jpg";
-    
-        const number = client.user.id;
         const sourceUrl = 'https://github.com/AbhishekSuresh2/Phoenix-MD';
-
-        const logo = await getBuffer(image);
-        const thumbnail = await getBuffer(thumb);
 
         const linkPreview = {
             title: "I'M Alive Now",
             body: "𝙿𝚑𝚘𝚎𝚗𝚒𝚡-𝙼𝙳",
-            thumbnail: logo,
+            thumbnailUrl: "https://i.ibb.co/tHWJrz3/IMG-20231128-WA0005.jpg",
             mediaType: 1,
             mediaUrl: sourceUrl,
             sourceUrl: sourceUrl,
@@ -35,7 +26,7 @@ module.exports = {
                 contactMessage: {
                     displayName: `${msg.pushName}`,
                     vcard: `BEGIN:VCARD\nVERSION:3.0\nN:XL;${client.user.name},;;;\nFN:${client.user.name},\nitem1.TEL;waid=919074692450:919074692450\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
-                    jpegThumbnail: thumbnail,
+                    jpegThumbnail: "https://i.ibb.co/tHWJrz3/IMG-20231128-WA0005.jpg",
                 }
             }
         };
