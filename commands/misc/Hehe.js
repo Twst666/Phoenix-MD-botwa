669f0b4545ca12744070030c27a3888d8aa71056
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-12-10 16:22:08 +0530

    Create Hehe.js

---
+++ commands/misc/Hehe.js
@@ -0,0 +1,23 @@
+const { OWNER_NAME, OWNER_NUMBER, BOT_NAME } = require('../../config');
+
+module.exports = {
+    name: "owner",
+    category: "main",
+    desc: "show the bot owner",
+    async exec({ msg }) {
+ const vcard = 'BEGIN:VCARD\n' 
+            + 'VERSION:3.0\n' 
+            + `FN:${OWNER_NAME}\n` 
+            + `ORG:${BOT_NAME};\n` 
+            + `TEL;type=CELL;type=VOICE;waid=${OWNER_NUMBER}:${OWNER_NUMBER}\n` 
+            + 'END:VCARD'
+return await client.sendMessage(
+    msg.from,
+    { 
+        contacts: { 
+            displayName: OWNER_NAME, 
+            contacts: [{ vcard }] 
+        }
+    }
+)
+});
