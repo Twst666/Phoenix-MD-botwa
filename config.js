3445daadcbff99651b29c9d965232d828d41426e
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-12-10 16:21:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,8 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID || 'Phoenix~WY6qDSff',
     HANDLERS: process.env.HANDLER || '.',
     BOT_NAME: process.env.BOT_NAME || 'Phoenix-MD',
+    OWNER_NAME: process.env.OWNER_NAME || 'Abhishek Suresh',
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '919074692450',
     SUDO: process.env.SUDO || '919074692450',
     STICKER_DATA: process.env.STICKER_DATA || '🎯𝙿𝚑𝚘𝚎𝚗𝚒𝚡-𝙼𝙳;𝙰𝚋𝚑𝚒𝚜𝚑𝚎𝚔 𝚂𝚞𝚛𝚎𝚜𝚑☘️',
 };
