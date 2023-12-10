0a195763af61a493bb80c202e73103c1fdb10aa8
Abhishek Suresh <104509404+AbhishekSuresh2@users.noreply.github.com>
2023-12-10 16:30:25 +0530

    Update Hehe.js

--- commands/misc/Hehe.js
+++ commands/misc/Hehe.js
@@ -4,20 +4,22 @@ module.exports = {
     name: "owner",
     category: "main",
     desc: "show the bot owner",
-    async exec({ msg }) {
- const vcard = 'BEGIN:VCARD\n' 
+    async exec({ msg, client }) {
+        const vcard = 'BEGIN:VCARD\n' 
             + 'VERSION:3.0\n' 
             + `FN:${OWNER_NAME}\n` 
             + `ORG:${BOT_NAME};\n` 
             + `TEL;type=CELL;type=VOICE;waid=${OWNER_NUMBER}:${OWNER_NUMBER}\n` 
-            + 'END:VCARD'
-return await client.sendMessage(
-    msg.from,
-    { 
-        contacts: { 
-            displayName: OWNER_NAME, 
-            contacts: [{ vcard }] 
-        }
+            + 'END:VCARD';
+
+        return await client.sendMessage(
+            msg.from,
+            { 
+                contacts: { 
+                    displayName: OWNER_NAME, 
+                    contacts: [{ vcard }] 
+                }
+            }
+        );
     }
-)
-});
+};
