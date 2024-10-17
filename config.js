8c4ef347448a3db769630e0bf8b4fd67340412b9
Twst666 <141984610+Twst666@users.noreply.github.com>
2024-10-17 11:28:49 +0530

    Update config.js by @AbhishekSuresh2

--- config.js
+++ config.js
@@ -3,11 +3,11 @@
 const toBool = (x) => x === 'true';
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'Phoenix~YJaZTBqy',
+    SESSION_ID: process.env.SESSION_ID || '',
     HANDLERS: process.env.HANDLER || '.',
     BOT_NAME: process.env.BOT_NAME || 'Phoenix-MD',
     OWNER_NAME: process.env.OWNER_NAME || 'Abhishek Suresh',
     OWNER_NUMBER: process.env.OWNER_NUMBER || '919074692450',
     SUDO: process.env.SUDO || '919074692450',
-    STICKER_DATA: process.env.STICKER_DATA || '🎯𝙿𝚑𝚘𝚎𝚗𝚒𝚡-𝙼𝙳;𝙰𝚋𝚑𝚒𝚜𝚑𝚎𝚔 𝚂𝚞𝚛𝚎𝚜𝚑☘️',
+    STICKER_DATA: process.env.STICKER_DATA || 'ᴘʜᴏᴇɴɪx-ᴍᴅ🎯 ᴀʙʜɪsʜᴇᴋ sᴜʀᴇsʜ🍀',
 };
